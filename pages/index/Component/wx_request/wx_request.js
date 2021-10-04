773ba4b89dd841d9953f8d7d4fe65bce52bd7317
景行 <oncwnuGDz8fF92HpUOgTVj7d9XUM@git.weixin.qq.com>
2021-10-04 18:05:38 +0800

    django环境搭建

---
+++ pages/index/Component/wx_request/wx_request.js
@@ -0,0 +1,23 @@
+// pages/index/Component/wx_request/wx_request.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
